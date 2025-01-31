c0de463825ff0d1e3f41e45a7ceab9e20445d202
AmEl <amirelkanov0504@gmail.com>
2025-01-31 15:08:27 +0300

    Version fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.amel.faerntourism"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.2.6"
+        versionCode = 8
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
